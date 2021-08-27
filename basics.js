316479f73487a73681ef7ccdf86993b52d3d8415
Miguel <migu0403@stud.kea.dk>
2021-08-27 09:38:50 +0200

    test2

---
+++ basics.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let y = 14;
+
+const bool = true;
+const num = 41;
+const str = "peter";
+const obj = { cats: 1, cars: 2 };
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
